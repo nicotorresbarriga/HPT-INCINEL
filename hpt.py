cd2dc9735b5b7a1d963d2c1de49f5f4dc5c74e9e
nicotorresbarriga <ntorresbarriga.1992@gmail.com>
2026-07-26 22:11:11 -0400

    Update hpt.py

--- hpt.py
+++ hpt.py
@@ -91,7 +91,7 @@ CENTROS_CORREOS = {"Centro Punta Vergara": "reportesrovincinel@gmail.com"}
 
 # PREVENCIÓN DESACTIVADA VISUALMENTE PARA PRUEBAS
 CORREOS_PREVENCION = ["No enviar (Modo Pruebas)", "No enviar (Modo Pruebas)"]
-CORREOS_OCULTOS = ["calarcon@incinel.cl", "ealvarez@incinel.cl"]
+CORREOS_OCULTOS = []
 
 RANGOS_INICIO = [f"{str(h).zfill(2)}:{str(m).zfill(2)}" for h in range(6, 12) for m in (0, 30)]  
 RANGO_TERMINO = [f"{str(h).zfill(2)}:{str(m).zfill(2)}" for h in range(16, 21) for m in (0, 30)] 
